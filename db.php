21f12b6ff24670c8b2c618e5aaab9a6e3850ebe2
safaex <133200932+safaex@users.noreply.github.com>
2023-12-01 11:14:47 +0100

    Add files via upload

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-
-class Database {
+class Database{
     public $pdo;
-
+ 
     public function __construct($db = "test", $user ="root", $pwd="", $host="localhost:3308") {
+ 
         try {
             $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,10 +11,35 @@ class Database {
           } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
           }
-
-
-
+       
+        }
+       
+ 
+        public function insertUser($email, $password){
+           $stmt = $this->pdo->prepare("insert into user(email, password) values (?, ?)");
+           $password = password_hash($password, PASSWORD_DEFAULT);
+           $stmt->execute([$email, $password]);
         }
 
-    }
+        public function select(){
+            $stmt = $this->pdo->query("SELECT * FROM user");
+            $resultaat = $stmt->fetchAll();
+            return $resultaat;
+        }
+ 
+        public function selectOneUser($id){
+            $stmt = $this->pdo->prepare("SELECT * FROM user WHERE id = ?");
+            $stmt->execute([$id]);
+            $resultaat = $stmt->fetch();
+            return $resultaat;
+        }
+ 
+}
+ 
+ 
+ 
+ 
+ 
+                                         
+ 
 ?>
--- home.php
+++ home.php
@@ -1,16 +1,45 @@
-
 <?php
-
-// Include het bestand waarin de Database class is gedefinieerd
 include 'db.php';
-
-// Maak een object van de Database class
 $db = new Database();
-
-// Nu kun je toegang krijgen tot de $pdo property van het $database object
 $pdo = $db->pdo;
 
-// Nu kun je queries uitvoeren met behulp van het $pdo object, bijvoorbeeld:
-// $result = $pdo->query("SELECT * FROM jouw_tabel");
-
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+        $db->insertUser($_POST['email'], $_POST['password']);
+        echo"Successfully added";
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+ 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+        <input type="text" name="email">
+        <input type="password" name="password">
+        <input type="submit">
+    </form>
+ 
+    <table border="2">
+        <tr>
+            <th>id</th>
+            <th>email</th>
+            <th>password</th>
+</tr>
+ 
+  <tr> <?php
+    $user = $db->selectOneUser(1); ?>
+    <td><?php echo $user['ID'];?></td>
+    <td><?php echo $user['email'];?></td>
+    <td><?php echo $user['password'];?></td>
+    </tr>
+  </table>
+</body>
+</html>
